9e5bbd4920eeec5f9dc1558826e9ef181125779b
ShindeOm91 <oshinde91199@gmail.com>
2024-08-19 13:13:52 +0530

    chnges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkeletonComponent } from './skeleton/skeleton.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { GeminiService } from './gemini.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SkeletonComponent,FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chat-bot';
+  prompt: string = '';
+
+  geminiService: GeminiService = inject(GeminiService);
+
+  loading: boolean = false;
+
+  chatHistory: any[] = [];
+  constructor() {
+    this.geminiService.getMessageHistory().subscribe((res) => {
+      if(res) {
+        this.chatHistory.push(res);
+      }
+    })
+  }
+
+  async sendData() {
+    if(this.prompt && !this.loading) {
+      this.loading = true;
+      const data = this.prompt;
+      this.prompt = '';
+      await this.geminiService.generateText(data);
+      this.loading = false;
+    }
+  }
+
+  formatText(text: string) {
+    const result = text.replaceAll('*', '');
+    return result;
+  }
 }
